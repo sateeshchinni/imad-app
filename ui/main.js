6a216d9fe5e23ce56e7aeb4acc6ab4e6beb55015
sateeshchinni <msgtosateesh@gmail.com>
2017-08-07 14:13:40 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ function moveRight(){
 
 img.onclick=function(){
     
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
     
 //    img.style.marginLeft = '100px';
 };
