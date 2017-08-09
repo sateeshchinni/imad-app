1db7fa1cb15357ebd774dd3f9eed13bcb29539ae
sateeshchinni <msgtosateesh@gmail.com>
2017-08-08 20:39:15 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,31 @@
-console.log('Loaded!');
+var button= document.getElementById('counter');
+var counter = 0;
 
-var element= document.getElementById("main-text");
-
-element.innerHTML = "New Value";
-
-var img=document.getElementById("madi");
-var marginLeft = 0 ;
-
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick=function(){
+button.onClick = function (){
+   counter = counter + 1;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
     
-    var interval = setInterval(moveRight,50);
-    
-//    img.style.marginLeft = '100px';
 };
+
+
+// console.log('Loaded!');
+
+// var element= document.getElementById("main-text");
+
+// element.innerHTML = "New Value";
+
+// var img=document.getElementById("madi");
+// var marginLeft = 0 ;
+
+// function moveRight(){
+//     marginLeft = marginLeft + 1;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
+
+// img.onclick=function(){
+    
+//     var interval = setInterval(moveRight,50);
+    
+// //    img.style.marginLeft = '100px';
+// };
