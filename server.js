ebdeafdc98a719652f19a4892dda244c5b647638
sateeshchinni <msgtosateesh@gmail.com>
2017-08-08 20:27:42 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,12 @@ app.get('/', function (req, res) {
 //   res.send(createTemplate(articleOne));
 // });
 
+var counter = 0;
+
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 
 app.get('/:articleName',function(req,res){
   //res.send('Artile one requested and will  be served here')  ;
