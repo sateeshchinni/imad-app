b8d0629c7a7e40fcc7f23ccadb078c8cc91182cf
sateeshchinni <msgtosateesh@gmail.com>
2017-08-08 20:46:54 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,30 @@
 var button= document.getElementById('counter');
-var counter = 0;
+//var counter = 0;
 
 button.onclick = function (){
-   counter = counter + 1;
+   
+   //create request
+   var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function (){
+     if(request.readystate === XMLHttpRequest.DONE) {
+         if(requst.status === 200){
+             var counter = request.responseText;
    var span = document.getElementById('count');
    span.innerHTML = counter.toString();
+             
+         }
+     }
+   };
+   
+   // make request
+   
+   request.open('GET','http://msgtosateesh.imad.hasura-app.io/counter',true);
+   request.send(null);
+   
+//   counter = counter + 1;
+//   var span = document.getElementById('count');
+//   span.innerHTML = counter.toString();
     
 };
 
