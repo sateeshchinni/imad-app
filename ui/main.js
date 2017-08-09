aed33d6959cee10dca50dcb975e621011cffbad7
sateeshchinni <msgtosateesh@gmail.com>
2017-08-08 22:29:22 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ button.onclick = function (){
      var list = '';
      
      for (var i=0;i<names.length;i++){
-         list = '<li>'+names[i]+'</li>';
+         list += '<li>'+names[i]+'</li>';
      }
      
      var url=document.getElementById('namelist');
