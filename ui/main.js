e9ff77db5d997ef6def94d5f562685083dff62b0
sateeshchinni <msgtosateesh@gmail.com>
2017-08-19 16:14:06 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -82,8 +82,8 @@ button.onclick = function (){
          }
      };
    
-   var username = document.getElementById('username');
-   var password = document.getElementById('password');
+   var username = document.getElementById('username').value;
+   var password = document.getElementById('password').value;
    
    console.log(username);
    console.log(password);
